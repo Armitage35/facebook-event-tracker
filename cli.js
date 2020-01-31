0ddf69fa3f0b5e0e8ad8d239d135606bc794e2c9
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2020-01-31 11:25:42 -0500

    Set POC up

--- cli.js
+++ cli.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const puppeteer = require('puppeteer');
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('https://www.facebook.com/0gravite/events/?ref=page_internal');
+	page.once('load', () => console.log('Page loaded!'));
+	const innerHTML = await page.evaluate(() => document.querySelector('#recurring_events_card').innerHTML);
+	console.log(innerHTML);
+	await page.screenshot({path: 'screenshots/example.png'});
+
+	await browser.close();
+})();
