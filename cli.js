a2cb8925469cda113f8a6c4af00f330609ea4c6d
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2020-02-04 11:20:07 -0500

    Setup POC up (#4)
    * Set POC up
    
    * Add Puppetteer doc
    
    * Parse recurring events
    
    * Cleanup and boilerplate normal events
    
    * Crawl past and recurring events
    
    * Please linter gods 🙏
    
    * Make links clickable
    
    * Style links
    
    * v0.1.0
    
    * Add eslint

--- cli.js
+++ cli.js
@@ -1,1 +1,39 @@
-console.log('Hello world');
+const puppeteer = require('puppeteer');
+const terminalLink = require('terminal-link');
+const chalk = require('chalk');
+
+const myPage = 'https://www.facebook.com/pg/brasserieboswell/events';
+
+// Works for past and upcoming events
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(myPage);
+
+	const eventTitles = await page.evaluate(
+		() => [...document.querySelectorAll('._50f7')].map(elem => elem.innerText.replace(/\n/g, ''))
+	);
+
+	const eventDates = await page.evaluate(
+		() => [...document.querySelectorAll('._5a5i')].map(elem => elem.innerText.replace(/\n/g, ''))
+	);
+
+	const eventLink = await page.evaluate(
+		() => [...document.querySelectorAll('._4dmk > a')].map(elem => elem.getAttribute('href'))
+	);
+
+	for (let i = 0; i < eventTitles.length; i++) {
+		console.log('Title: ' + eventTitles[i]);
+		console.log('Date: ' + eventDates[i]);
+		console.log(
+			chalk.blue(
+				chalk.underline(
+					terminalLink('Link to the event', 'https://facebook.com/' + eventLink[i])
+				)
+			)
+		);
+		console.log('\n');
+	}
+
+	await browser.close();
+})();
