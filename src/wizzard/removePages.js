985450a917fbef763800a1c859439a220706c2da
Restyled.io <commits@restyled.io>
2020-03-28 18:39:32 +0000

    Restyled by prettier

--- src/wizzard/removePages.js
+++ src/wizzard/removePages.js
@@ -7,16 +7,20 @@ const welcomeUser = require('./welcomeUser');
 const saveTrackedPages = require('../saveTrackedPages');
 
 const removePages = () => {
-	inquirer.prompt([{
-		type: 'list',
-		name: 'nextStep',
-		message: strings.english.welcomeWizzard.removePages.question,
-		choices: settings.pages
-	}]).then(answers => {
-		settings.pages.splice(settings.pages.indexOf(answers.nextStep),1);
-		saveTrackedPages();
-		welcomeUser();
-	});
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'nextStep',
+				message: strings.english.welcomeWizzard.removePages.question,
+				choices: settings.pages,
+			},
+		])
+		.then(answers => {
+			settings.pages.splice(settings.pages.indexOf(answers.nextStep), 1);
+			saveTrackedPages();
+			welcomeUser();
+		});
 };
 
 module.exports = removePages;
