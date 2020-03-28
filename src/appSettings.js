3025be532af9afb0919825d645defa0d7be31fc0
Restyled.io <commits@restyled.io>
2020-03-28 18:54:21 +0000

    Restyled by prettier

--- src/appSettings.js
+++ src/appSettings.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 const settings = {
-	parallel : 4, // handles how many tabs Chrome will open at once
-	pages : [],
-	pathToSavedPages : path.join(__dirname, 'pages.csv'), // eslint-disable-line no-undef
-	maxMonthInFuture: 3
+	parallel: 4, // handles how many tabs Chrome will open at once
+	pages: [],
+	pathToSavedPages: path.join(__dirname, 'pages.csv'), // eslint-disable-line no-undef
+	maxMonthInFuture: 3,
 };
 
 module.exports = settings;
