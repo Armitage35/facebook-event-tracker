2c311fb6306d67da639d9fcd125606fe5e1b8fdb
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2020-03-28 14:39:19 -0400

    Disable one eslint rule

--- src/appSettings.js
+++ src/appSettings.js
@@ -3,7 +3,7 @@ const path = require('path');
 const settings = {
 	parallel : 4, // handles how many tabs Chrome will open at once
 	pages : [],
-	pathToSavedPages : path.join(__dirname, 'pages.csv'),
+	pathToSavedPages : path.join(__dirname, 'pages.csv'), // eslint-disable-line no-undef
 	maxMonthInFuture: 3
 };
 
