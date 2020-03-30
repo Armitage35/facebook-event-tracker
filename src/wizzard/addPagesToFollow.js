05987da74b329e9361c42e83b2ffe71572ba7193
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2020-03-30 10:00:30 -0400

    Get ready for new modularization

--- src/wizzard/addPagesToFollow.js
+++ src/wizzard/addPagesToFollow.js
@@ -13,7 +13,7 @@ const addPagesToFollow = () => {
 		message: strings.english.welcomeWizzard.addPages.question,
 		validate: function (value) {
 			let pass = value.match(
-				/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g
+				/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g // lgtm [js/regex/duplicate-in-character-class]
 			);
 			if (pass) {
 				return true;
